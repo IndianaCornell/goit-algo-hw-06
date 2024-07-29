aad263b85a99a888617416ca882d625ba59c9775
Serj Pyshkin <IndianaCornell@gmail.com>
2024-07-29 17:40:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,11 +61,45 @@ bfs_result = bfs_path(G, "Park", "Lviv Theatre")
 
 
 
-# Дейкстра
-def dijkstra_path(graph, start, end):
-    return nx.dijkstra_path(graph, start, end)
+def dijkstra(graph, start):
+    # Ініціалізація
+    shortest_paths = {start: (None, 0)}
+    current_node = start
+    visited = set()
 
-shortest_path = dijkstra_path(G, "Park", "Lviv Theatre")
+    while current_node is not None:
+        visited.add(current_node)
+        destinations = graph.neighbors(current_node)
+        weight_to_current_node = shortest_paths[current_node][1]
+
+        for next_node in destinations:
+            weight = graph[current_node][next_node]['weight'] + weight_to_current_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight)
+            else:
+                current_shortest_weight = shortest_paths[next_node][1]
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight)
+
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            break
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+
+    return shortest_paths
+
+def shortest_path(graph, start, goal):
+    shortest_paths = dijkstra(graph, start)
+    path = []
+    current_node = goal
+    while current_node is not None:
+        path.append(current_node)
+        next_node = shortest_paths[current_node][0]
+        current_node = next_node
+    path = path[::-1]
+    return path
+
+shortest_path_result = shortest_path(G, "Park", "Lviv Theatre")
 
 
 
@@ -96,4 +130,4 @@ print(f"BFS шлях з Парку до Оперного театру: {bfs_resu
 
 print()
 
-print(f"Найкоротший шлях (Дейкстра) з Парку до Оперного театру: {shortest_path}")
+print(f"Найкоротший шлях (Дейкстра) з Парку до Оперного театру: {shortest_path_result}")
